9faef3299ba4dffc997018b33c35c56f8661038f
Aoikaze4 <kaze4border@gmail.com>
2021-06-14 16:54:03 +0000

    [update]header model routes

--- Booker2/config/routes.rb
+++ Booker2/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :books, only:[:index, :edit, :show, :new]
+  devise_for :users
+  root to: 'homes#top'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
