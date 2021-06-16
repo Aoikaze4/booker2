c3c0701d7da8ecb8763a43adb1d23d13d3c16b27
Aoikaze4 <kaze4border@gmail.com>
2021-06-16 15:36:14 +0000

    [update]create header,footer

--- Booker2/config/routes.rb
+++ Booker2/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :books, only:[:index, :edit, :show, :new]
+
   devise_for :users
+
   root to: 'homes#top'
+  get 'home/about', to: 'homes#about'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
